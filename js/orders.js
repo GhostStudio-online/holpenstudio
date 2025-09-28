e62510d271e4c013d47f4b087784ecf3826fc666
GhostStudio <mrpantersschannel@gmail.com>
2025-09-28 17:44:42 +0300

    Create orders.js

---
+++ js/orders.js
@@ -0,0 +1,22 @@
+const ordersec = document.getElementById('orders');
+
+async function loadOrders() {
+    const responce = await fetch('https://5ef8bbba3091.ngrok-free.app/api/orders');
+    const orders = await responce.json();
+
+    const orderlist = orders.map(order => `
+        <div class="flex flex-col relative p-2 border border-gray-400/60 rounded-lg">
+            <div class="w-[100%]">
+                <img src="${order.icon}" alt="${order.alt}" class="w-full h-full rounded-lg">
+            </div>
+            <h2 class="text-gray-100 text-xl font-semibold tracking-tight">${order.name}</h2>
+            <h3 class="text-gray-300 text-md font-semibold tracking-tight">Цена: ${order.price}₴</h3>
+            <h4 class="px-2 py-1 bg-blue-600 text-sm text-white rounded-md absolute top-[3%] right-[2%]">${order.status}</h4>
+        </div>
+    `).join('');
+
+    ordersec.innerHTML = orderlist;
+}
+
+
+loadOrders();
