383f92068fbb4d98b60b0c6564b6c4b88ac763ba
GhostStudio <mrpantersschannel@gmail.com>
2025-09-29 16:18:10 +0300

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,7 +1,7 @@
 const ordersec = document.getElementById('orders');
 
 async function loadOrders() {
-    const responce = await fetch('http://4.tcp.eu.ngrok.io:19293/api/orders');
+    const responce = await fetch('holpenstudioback-production.up.railway.app/api/orders');
     const orders = await responce.json();
 
     const orderlist = orders.map(order => `
