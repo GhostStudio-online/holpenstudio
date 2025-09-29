a080180147bca1ed0ee6fd2925792fdadc1ed24a
GhostStudio <mrpantersschannel@gmail.com>
2025-09-29 16:18:21 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 const productsection = document.getElementById('productscont');
 
 async function loadProducts() {
-    const responce = await fetch('http://4.tcp.eu.ngrok.io:19293/api/products');
+    const responce = await fetch('https://holpenstudioback-production.up.railway.app/api/products');
     const products = await responce.json();
 
     const productslist = products.map(product => `
@@ -47,4 +47,5 @@ async function loadProducts() {
 }
 
 loadProducts();
+
 
