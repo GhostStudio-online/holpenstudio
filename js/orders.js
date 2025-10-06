f298bbd33b0ed520d02a40941a0493088853ad56
GhostStudio <mrpantersschannel@gmail.com>
2025-10-06 23:22:57 +0300

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,7 +1,7 @@
 const ordersec = document.getElementById('orders');
 
 async function loadOrders() {
-    const responce = await fetch('https://holpenstudioback-production.up.railway.app/api/orders');
+    const responce = await fetch('https://holpenstudioback-production.up.railway.app/api/orders/get');
     const orders = await responce.json();
 
     const orderlist = orders.map(order => `
@@ -17,6 +17,7 @@ async function loadOrders() {
 
     ordersec.innerHTML = orderlist;
 }
+
 
 
 loadOrders();
